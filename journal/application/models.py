16f8c9a38bd5a92a8a557759bdf3d401448ce8f9
Alex-Bane <ABane@QA.com>
2020-11-12 14:50:10 +0000

    updated models.py

--- journal/application/models.py
+++ journal/application/models.py
@@ -6,12 +6,12 @@ class ToDoList(db.Model):
 	complete= db.Column(db.Boolean, default=False)
 
 class Users(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, primary_key=True)
 	user_name = db.Column(db.String(30), nullable = False)
 	
 	
 class Entries(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	entry = db.Column(db.String(1000), nullable=False)
-	fk_user_id = db.Column('id', db.Integer, db.ForeignKey('Users.id'))
+	fk_user_id = db.Column('id', db.Integer, db.ForeignKey('Users.user_id'))
 	fk_user_name = db.Column('user_name', db.String(30), db.ForeignKey('Users.user_name'))
